7c104b5be8933aa34330b5fc044e14127ec8c8b4
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-27 14:32:24 +0800

    feat: :sparkles: add `Avatar` component

---
+++ components/avatar/Avatar.tsx
@@ -0,0 +1,30 @@
+import ContentfulImage from "@/lib/contentful-image";
+
+import { type AvatarProps } from "./types";
+
+/**
+ * Avatar component displays an avatar with a name.
+ *
+ * @param name - The name of the avatar.
+ * @param picture - The picture object of the avatar.
+ *
+ * @returns The rendered Avatar component.
+ */
+export const Avatar = ({ name, picture }: AvatarProps): JSX.Element => {
+  return (
+    <div className="flex items-center">
+      <div className="mr-4 w-12 h-12">
+        <ContentfulImage
+          alt={name}
+          className="object-cover h-full rounded-full"
+          height={48}
+          width={48}
+          src={picture.url || "#"}
+        />
+      </div>
+      <div className="text-xl font-bold">{name}</div>
+    </div>
+  );
+};
+
+Avatar.displayName = "Avatar";
---
+++ components/avatar/types.d.ts
@@ -0,0 +1,6 @@
+import { PictureAuthorData } from "../layouts/more-stories/types";
+
+export type AvatarProps = {
+  name: string;
+  picture: PictureAuthorData;
+};
