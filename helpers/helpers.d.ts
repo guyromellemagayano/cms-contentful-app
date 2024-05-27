fac67d9419c1fd936098bbd0a0cd1d841a7b1830
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-27 14:40:44 +0800

    feat: :sparkles: add styles helper function

---
+++ helpers/helpers.d.ts
@@ -0,0 +1,1 @@
+export type CnProps = Array<any>;
---
+++ helpers/styles.ts
@@ -0,0 +1,12 @@
+import { CnProps } from "./helpers";
+
+/**
+ * Combines multiple class names into a single string, filtering out any falsy values.
+ *
+ * @param classes - The class names to combine.
+ *
+ * @returns A string containing the combined class names.
+ */
+export const cn = (...classes: CnProps) => {
+  return classes.filter(Boolean).join(" ");
+};
