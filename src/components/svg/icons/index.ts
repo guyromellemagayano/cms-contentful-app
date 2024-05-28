e226ea40988b6f39525b54de3cdfaa05f9b4ea0a
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-29 06:08:17 +0800

    feat: :sparkles: add SVG icons and export file

---
+++ src/components/svg/icons/index.ts
@@ -0,0 +1,3 @@
+export { default as ColorfulCoinLogo } from "./colorful-coin-logo.svg";
+export { default as LogoTagline } from "./logo-tagline.svg";
+export { default as SettingsIcon } from "./settings-icon.svg";
