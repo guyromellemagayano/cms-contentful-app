748adb896c7445153217c95cace0c1fe1ede2816
Guy Romelle Magayano <aspiredtechie2010@gmail.com>
2024-05-29 01:04:14 +0800

    refactor: :recycle: update app providers

--- src/app/[locale]/providers.tsx
+++ src/app/[locale]/providers.tsx
@@ -1,39 +1,26 @@
 "use client";
 
-import { useState } from "react";
-
-import { QueryCache, QueryClient } from "@tanstack/react-query";
-import { useRouter } from "next/navigation";
-
-import { useContentfulContext } from "@/hooks/contentful";
-
-import type { ParamsData } from "./layout";
+import ContentfulContentProvider from "@/contexts/contentful";
+import LivePreviewProvider from "@/contexts/live-preview";
+import MuiProvider from "@/contexts/mui";
+import ReactQuery from "@/contexts/react-query";
+// import { useContentfulContext } from "@/hooks/contentful";
+import type { ParamsData } from "@/types/page";
 
 export type ProvidersProps = Pick<ParamsData, "children">;
 
 const Providers = ({ children }: ProvidersProps) => {
-  const queryConfig = {
-    queryCache: new QueryCache({
-      onError: () => {
-        router.push("/404");
-      },
-    }),
+  // const { previewActive } = useContentfulContext();
 
-    defaultOptions: {
-      queries: {
-        retry: false,
-        refetchOnMount: false,
-        refetchIntervalInBackground: false,
-        refetchOnWindowFocus: false,
-      },
-    },
-  };
-
-  const [queryClient] = useState(() => new QueryClient(queryConfig)),
-    { previewActive } = useContentfulContext(),
-    router = useRouter();
-
-  return children;
+  return (
+    <ContentfulContentProvider>
+      <LivePreviewProvider>
+        <ReactQuery>
+          <MuiProvider>{children}</MuiProvider>
+        </ReactQuery>
+      </LivePreviewProvider>
+    </ContentfulContentProvider>
+  );
 };
 
 Providers.displayName = "Providers";
